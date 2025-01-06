bfec2bc9f49950540dbb7dc942d7be39970079b1
Bailey Kocin <bailey.kocin@gmail.com>
2025-01-05 23:42:09 -0500

    Add project stuff

---
+++ Core/Lilac.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Native;
+
+namespace Core;
+
+public static class Lilac
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Starting Application!");
+        DynamicLibrary library = new DynamicLibrary("kernel32.dll");
+        Console.WriteLine(library.IsInvalid);
+        Console.WriteLine("Ending Application!");
+    }
+}
